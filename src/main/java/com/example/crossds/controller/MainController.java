021bd249d2d8fa67a84a9fbe21167a1d8b50abbf
hugodesb <hugo.desbiolles@gmail.com>
2020-09-10 19:16:13 +0200

    add hello function

--- src/main/java/com/example/crossds/controller/MainController.java
+++ src/main/java/com/example/crossds/controller/MainController.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -20,6 +22,11 @@ public class MainController {
 
     @Autowired
     private DeezerApiService deezerApiService;
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name){
+        return "Hello, "+name;
+    }
 
     public GenericApiService getApiService(Platform platform){
         switch (platform){
